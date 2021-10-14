68b6cc923b1bb8f9593e6fa92f0a72dd07795b69
Mattias Plaisier <mattias.plaisier@flowpilots.com>
2021-10-14 18:13:05 +0200

    Init project

---
+++ Climber.Forms.Core/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using FreshMvvm;
+using Xamarin.Forms;
+
+namespace Climber.Forms.Core
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var page = FreshPageModelResolver.ResolvePageModel<MainViewModel>();
+            var basicNavContainer = new FreshNavigationContainer(page);
+            MainPage = basicNavContainer;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Climber.Forms.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,9 @@
+﻿using FreshMvvm;
+
+namespace Climber.Forms.Core
+{
+    public class MainViewModel : FreshBasePageModel
+    {
+        public string HelloWorld { get; } = "Hello World";
+    }
+}
